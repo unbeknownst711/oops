9eef18565964d962b1305bd7a9f6fed1dc572679
unbeknownst711 <teswayae@gmail.com>
2025-09-17 09:27:59 +0700

    ----

--- src/tutorialClass/Shape.java
+++ src/tutorialClass/Shape.java
@@ -1,18 +1,13 @@
 package tutorialClass;
 
 public class Shape {
-    private String _color;
-
+    private final String _color;
     public Shape(String color) {
         this._color = color;
     }
 
     public String get_color() {
         return _color;
-    }
-
-    public void set_color(String _color) {
-        this._color = _color;
     }
     public double getArea(){
         return 0;
